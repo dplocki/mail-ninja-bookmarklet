00d9d19ada7d7d2b1655a7fbca217b22870a0152
Ziomioslaw <dawid.plocki@gmail.com>
2023-02-18 12:34:38 +0100

    Introducint typescript into the project

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const gulp = require('gulp');
 const wrap = require('gulp-wrap');
 const uglify = require('gulp-uglify');
+const ts = require('gulp-typescript').createProject('tsconfig.json');
 
 gulp.task('default', function() {
-  return gulp.src('code.js')
+  return gulp.src('src/code.ts')
+    .pipe(ts())
     .pipe(uglify())
     .pipe(wrap('javascript:(<%= contents %>(location.href));'))
     .pipe(gulp.dest('out'));
--- src/code.ts
+++ src/code.ts
@@ -1,15 +1,18 @@
+declare var window: Window & typeof globalThis;
+declare var document: Document;
+
 function selectUnreadEmails(link) {
   if (/^https:\/\/poczta\.interia\.pl.*/.test(link)) {
     for (const row of document.querySelectorAll("tr.ch")) {
-      row.children[0].children[0].click();
+      (row.children[0].children[0] as HTMLElement).click();
     }
   } else if (/^https?:\/\/poczta\.onet\.pl.*/.test(link)) {
     for (const checkbox of document.querySelectorAll('.single-mail-row.unread .checklist')) {
-      checkbox.click();
+      (checkbox as HTMLElement).click();
     }
   } else if (/^https?:\/\/poczta\.wp\.pl.*/.test(link)) {
     for (const checkbox of document.querySelectorAll('div.StreamItem div.stream-item__wrap.is-unread .Checkbox')) {
-      checkbox.click();
+      (checkbox as HTMLElement).click();
     }
   } else {
     alert("Nie znana poczta")
