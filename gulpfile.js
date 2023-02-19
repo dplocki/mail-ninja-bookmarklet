96ef6ac78092ed8517a377488d13d62f77f63c91
Ziomioslaw <dawid.plocki@gmail.com>
2023-02-19 17:32:54 +0100

    Add html version for final product

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 const gulp = require('gulp');
 const wrap = require('gulp-wrap');
 const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 const ts = require('gulp-typescript').createProject('tsconfig.json');
 
 gulp.task('default', function() {
   return gulp.src('src/code.ts')
     .pipe(ts())
     .pipe(uglify())
-    .pipe(wrap('javascript:(function(){<%= contents %>})()'))
+    .pipe(wrap({ src: './src/index.html' }))
+    .pipe(rename({ basename: 'index', extname: '.html' }))
     .pipe(gulp.dest('out'));
 });
