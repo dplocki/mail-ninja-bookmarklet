ded3eddb49d1167b7b3ac6a7c5895464f1ee818f
Ziomioslaw <dawid.plocki@gmail.com>
2023-02-19 14:01:23 +0100

    Remove unused code

--- src/code.ts
+++ src/code.ts
@@ -2,7 +2,6 @@ declare var window: Window & typeof globalThis;
 declare var document: Document;
 
 interface IEmail {
-  title: string,
   selector: HTMLElement
 }
 
@@ -12,7 +11,6 @@ function getInteriaEmails(document: Document): IEmail[] {
   return Array.from(document.getElementsByClassName('msglist-item'))
     .filter((item: Element) => !Array.from(item.classList).includes('msglist-item--seen'))
     .map<IEmail>((item: Element) => ({
-      title: '',
       selector: item.getElementsByTagName('label')[0] as HTMLElement
     }));
 }
@@ -20,7 +18,6 @@ function getInteriaEmails(document: Document): IEmail[] {
 function getOnetEmails(document: Document): IEmail[] {
   return Array.from(document.querySelectorAll('.single-mail-row.unread .checklist'))
     .map<IEmail>((item: Element) => ({
-      title: '',
       selector: item as HTMLElement
     }));
 }
@@ -28,7 +25,6 @@ function getOnetEmails(document: Document): IEmail[] {
 function getWpEmails(document: Document): IEmail[] {
   return Array.from(document.querySelectorAll('div.StreamItem div.stream-item__wrap.is-unread .Checkbox'))
     .map<IEmail>((item: Element) => ({
-      title: '',
       selector: item as HTMLElement
     }));
 }
@@ -42,7 +38,7 @@ function selectUnreadEmails(document: Document, link: string): void {
 
   const matchingEmail: string | undefined = Object.keys(cases).find(emailLink => (new RegExp(`^${emailLink}.*`)).test(link));
   if (!matchingEmail) {
-    alert("Nie znana poczta");
+    alert("Nieznana poczta");
     return;
   }
 
