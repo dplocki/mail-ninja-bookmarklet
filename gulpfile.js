8c123618942eb84d222e89e323482dff909a3451
Ziomioslaw <dawid.plocki@gmail.com>
2023-02-20 20:27:49 +0100

    Add index.html as artifact template

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,13 @@ const wrap = require('gulp-wrap');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const ts = require('gulp-typescript').createProject('tsconfig.json');
+const packageJson = require('./package.json');
 
 gulp.task('default', function() {
   return gulp.src('src/code.ts')
     .pipe(ts())
     .pipe(uglify())
-    .pipe(wrap({ src: './src/index.html' }))
+    .pipe(wrap({ src: './src/index.html' }, { version: packageJson.version }))
     .pipe(rename({ basename: 'index', extname: '.html' }))
     .pipe(gulp.dest('out'));
 });
